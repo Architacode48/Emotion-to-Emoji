79a46c49b68f0d5f0242026fbc9bd3d36955bf56
Archita Pandey <71716706+Architacode48@users.noreply.github.com>
2020-11-20 19:25:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ png_quality:90
 camera=document.getElementById("camera");
 Webcam.attach('#camera');
 
-function click(){
+function takePhoto(){
 Webcam.snap(function(data_uri){
 document.getElementById("result").innerHTML='<img id="image"src="'+data_uri+'"/>';
 }
 );}
 console.log('ml5 version',ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/yLdbguVu_/model.json',modelLoaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/GkpTDO10R/model.json',modelLoaded);
 function modelLoaded(){
     console.log("model has loaded");
 }
@@ -28,25 +28,36 @@ function gotResult(error,results){
 console.log(results);
 document.getElementById("emotion_name").innerHTML=results[0].label;
 document.getElementById("emotion_name2").innerHTML=results[1].label;
+prediction1=results[0].label;
+prediction2=results[1].label;
+speak();
 if(results[0].label=="Sleepy"){
-document.getElementById("emoji").innerHTML="&#128558;";
+document.getElementById("emoji").innerHTML="&#128554;";
 }
 if(results[0].label=="Happy"){
 document.getElementById("emoji").innerHTML="&#128512;";
 }
-if(results[0].label=="scared"){
-    document.getElementById("emoji").innerHTML="&#128560;";
+if(results[0].label=="Surprise"){
+    document.getElementById("emoji").innerHTML="&#128558;";
     }
 if(results[1].label=="Sleepy"){
- document.getElementById("emoji2").innerHTML="&#128558;";
+ document.getElementById("emoji2").innerHTML="&#128554;";
 }
  if(results[1].label=="Happy"){
  document.getElementById("emoji2").innerHTML="&#128512;";
 }
-if(results[1].label=="scared"){
-document.getElementById("emoji2").innerHTML="&#128560;";
+if(results[1].label=="Surprise"){
+document.getElementById("emoji2").innerHTML="&#128558;";
  }
-
-
     }
+}
+function speak()
+{
+    var synth=window.speechSynthesis;
+    speakData1="The first prediction is "+ prediction1;
+    speakData2="The second prediction is"+ prediction2;
+    var utterThis= new SpeechSynthesisUtterance(speakData1+speakData2);
+   utterThis.rate=1.0;
+    synth.speak(utterThis);
+
 }
